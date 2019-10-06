58153e1b30993c61c050e7173eaf8982e864d6c7
scirop <swarupsahoo@icloud.com>
2019-10-06 04:09:05 -0500

    removed __main__ check

--- app.py
+++ app.py
@@ -7,10 +7,4 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///soda_machines.db'
 db = SQLAlchemy(app)
 
-if __name__ == "__main__":
-    # We need to make sure Flask knows about its views before we run
-    # the app, so we import them. We could do it earlier, but there's
-    # a risk that we may run into circular dependencies, so we do it at the
-    # last minute here.
-    from views import *
-    app.run()
+from views import *
