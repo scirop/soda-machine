84146d65d4570a5c238697e9b9241a9f4f8b3d17
scirop <swarupsahoo@icloud.com>
2019-10-06 04:13:10 -0500

    commented main

--- app.py
+++ app.py
@@ -7,4 +7,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///soda_machines.db'
 db = SQLAlchemy(app)
 
+# We need to make sure Flask knows about its views before we run
+# the app, so we import them. We could do it earlier, but there's
+# a risk that we may run into circular dependencies, so we do it at the
+# last minute here.
+
 from views import *
+
+#if __name__ == "__main__":
+    #from views import *
+    #app.run()
